a5ab2218f80fce7a7820c3389961ef0de1952a28
Huang <sharkmanhmz@tamu.edu>
2021-04-21 12:50:56 -0500

    For different visualizations.

---
+++ MeanAE_NB_mlbd.R
@@ -0,0 +1,30 @@
+# Libraries
+library(tidyverse)
+library(hrbrthemes)
+library(viridis)
+library(plotly)
+data <- data.frame(NB_eval_3d[ , , 6])
+rownames(data) <- as.character(seq(-0.9, 0.9, by = 0.1))
+colnames(data) <- as.character(seq(0.1, 0.8, by = 0.1))
+
+library(heatmaply)
+MeanAE_NB_mlbd <- heatmaply(data, 
+                          dendrogram = "none",
+                          xlab = "TruncRate", ylab = "LatentR", 
+                          main = "Mean Absolute Error for Ternary/Binary (mlbd)",
+                          #scale = "column",
+                          margins = c(60,100,40,20),
+                          grid_color = "white",
+                          grid_width = 0.00001,
+                          titleX = TRUE,
+                          hide_colorbar = FALSE,
+                          branches_lwd = 0.1,
+                          label_names = c("LatentR:", "TrucRate:", "MeanAE:"),
+                          fontsize_row = 5, fontsize_col = 5,
+                          labCol = colnames(data),
+                          labRow = rownames(data),
+                          heatmap_layers = theme(axis.line=element_blank())
+)
+# save the widget
+library(htmlwidgets)
+saveWidget(MeanAE_NB_mlbd, file= "MeanAE_NB_mlbd.html")
