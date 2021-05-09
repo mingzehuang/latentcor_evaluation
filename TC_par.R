2426958a901fe38267b0598abfd78f673b42cc70
Mingze Huang <mingzehuang@gmail.com>
2021-05-09 07:19:30 -0500

    Update grid

--- TC_par.R
+++ TC_par.R
@@ -13,7 +13,7 @@ source("/scratch/user/sharkmanhmz/latentcor_git/latentcor/R/bridge.R")
 #For TC Case
 # grid values that used to create precomputed values
 tau_grid <- round(pnorm(seq(-2.1, 2.1, by = .15)), 6) * 2 - 1
-d1_grid <- log10(seq(1.1, 10^0.99, length = 15))
+d1_grid <- round(pnorm(seq(.15, 2.1, by = .15)), 6) * 2 - 1
 l_tau_grid <- length(tau_grid); l_d1_grid <- length(d1_grid)
 TCvalue <- matrix(NA, l_tau_grid, l_d1_grid)
 
