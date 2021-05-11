cc2e8a5468631aa4dbd3df0e2e4dd66c030a0dd9
Mingze Huang <mingzehuang@gmail.com>
2021-05-11 02:09:09 -0500

    Add interpolation for ternary/truncated case

--- all_par.R
+++ all_par.R
@@ -5,7 +5,8 @@ load("/scratch/user/sharkmanhmz/latentcor_evaluation_git/latentcor_evaluation/TB
 load("/scratch/user/sharkmanhmz/latentcor_evaluation_git/latentcor_evaluation/TT_grid.rda")
 load("/scratch/user/sharkmanhmz/latentcor_evaluation_git/latentcor_evaluation/NC_grid.rda")
 load("/scratch/user/sharkmanhmz/latentcor_evaluation_git/latentcor_evaluation/NB_grid.rda")
+load("/scratch/user/sharkmanhmz/latentcor_evaluation_git/latentcor_evaluation/NT_grid.rda")
 load("/scratch/user/sharkmanhmz/latentcor_evaluation_git/latentcor_evaluation/NN_grid.rda")
 
-save(BCipol, BBipol, TCipol, TBipol, TTipol, NCipol, NBipol, NNipol, file = "all_grid.rda",
+save(BCipol, BBipol, TCipol, TBipol, TTipol, NCipol, NBipol, NTipol, NNipol, file = "all_grid.rda",
      compress = "xz")
