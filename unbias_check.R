7a6884dbfb95f089b36a1fc63c9fec6c8697310a
Mingze Huang <mingzehuang@gmail.com>
2021-06-07 13:52:44 -0500

    Move unbias check out of package itself and put into numerical evaluation.

---
+++ unbias_check.R
@@ -0,0 +1,39 @@
+library(latentcor)
+### Data setting
+n <- 1000; p1 <- 15; p2 <- 10 # sample size and dimensions for two datasets.
+maxcancor <- 0.9 # true canonical correlation
+
+### Correlation structure within each data set
+set.seed(0)
+perm1 <- sample(1:(p1 + p2), size = p1 + p2);
+Sigma <- autocor(p1 + p2, 0.7)[perm1, perm1]
+# blockind <- sample(1:3, size = p2, replace = TRUE);
+# Sigma2 <- blockcor(blockind, 0.7)
+mu <- rbinom(p1 + p2, 1, 0.5)
+
+cp1 <- "exp"; cp2 <- "cube"
+for (tp1 in c("continuous", "binary", "ternary", "trunc")) {
+  for (tp2 in c("continuous", "binary", "ternary", "trunc")) {
+    for (md in c("original", "ml", "approx")) {
+      c1 <- c2 <- NULL
+      if (tp1 == "binary" | tp1 == "trunc") {
+        c1 <- rep(1, p1)
+      } else if (tp1 == "ternary") {
+        c1 <- matrix(rep(1:2, p1), nrow = 2, ncol = p1)
+      }
+      if (tp2 == "binary" | tp2 == "trunc") {
+        c2 <- rep(0, p2)
+      } else if (tp2 == "ternary") {
+        c2 <- matrix(rep(0:1, p2), nrow = 2, ncol = p2)
+      }
+      simdata <- GenData(n=n, copula1 = cp1, copula2 = cp2, type1 = tp1, type2 = tp2,
+                         muZ = mu, Sigma = Sigma, p1= p1, p2 = p2, c1 = c1, c2 = c2)
+      X1 <- simdata$X1; X2 <- simdata$X2
+      assign(paste("R", cp1, cp2, tp1, tp2, md, sep = "_"),
+             estR(X1 = X1, type1 = tp1, X2 = X2, type2 = tp2, method = md)$R)
+      PlotPair(datapair = cbind(c(Sigma), c(get(paste("R", cp1, cp2, tp1, tp2, md, sep = "_")))),
+               namepair = c("Sigma", paste("R", cp1, cp2, tp1, tp2, md, sep = "_")),
+               title = "Latent correlation (True vs. Estimated)")
+    }
+  }
+}
