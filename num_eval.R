d287e76e68b27f501c501e2a52fccb875b15aa48
Mingze Huang <mingzehuang@gmail.com>
2021-06-23 16:29:41 -0500

    Modify unbias_check

--- num_eval.R
+++ num_eval.R
@@ -23,29 +23,20 @@ type1 = "binary"; type2 = "continuous"
 # will test 19 latent r and 9 zero proportion values.
 latentRseq = seq(-0.9, 0.9, by = 0.1)
 zratioseq = seq(0.1, 0.9, by = 0.1)
+types = c("continuous", "binary", "trunc", "ternary")
 methods = c("original", "ml", "approx")
-indseq = expand.grid(latentRseq, zratioseq, methods)
-bench = "original"
-bm = which(methods == bench)
+indseq = expand.grid(nrep, latentRseq, zratioseq, types, types, methods)
 registerDoFuture()
 plan(multicore, workers = 80)
-
 value =
-foreach (ind = 1:nrow(indseq)) %dopar% {
-  # initialize for every combination
-  time = Kcor = matrix(NA, nrow = nrep, ncol = length(methods))
-  for(r in 1:nrep){
-      simdata = GenData(n = n, type1 = type1, type2 = type2, rho = latentRseq[trueR], q1 = zratioseq[zrate])
-      X1 = simdata$X1; X2 = simdata$X2
-      for (m in 1:length(methods)) {
-        time[r, m] = median(microbenchmark::microbenchmark(Kcor[i, m] = estimateR_mixed(X1 = x1, X2 = x2, type1 = type1, type2 = type2, method = methods[m])$R12, times = 5)$time) / 10^6
-      }
-    }
-    value = c(apply(time, 2, median), colMeans(abs(Kcor - latentRseq[trueR])), colMeans(abs(Kcor - Kcor[bm])), apply(abs(Kcor - latentRseq[trueR]), 2, max), apply(abs(Kcor - Kcor[bm]), 2, max))
-  }
-
+foreach (ind = 1:nrow(indseq)) {
+  simdata = GenData(n = n, type1 = type1, type2 = type2, rho = latentRseq[trueR], q1 = zratioseq[zrate])
+  X1 = simdata$X1; X2 = simdata$X2
+  time = median(microbenchmark::microbenchmark(estimate = estR(X1 = x1, X2 = x2, type1 = type1, type2 = type2, method = methods[m])$R12, times = 5)$time) / 10^6
+  value = c(time, estimate)
+}
 out = array(NA, c(length(latentRseq), length(zratioseq), length(methods), (length(methods)) ^ 3 * (length(methods) - 1) ^ 2))
 for (j in 1:nrow(indseq)) {
-  out[indseq[j, 1], indseq[j, 2], indseq[j, 3], ] = value[[j]]
+  out[indseq[j, 1], indseq[j, 2], indseq[j, 3], ] = value[j]
 }
 save(out, file = "out.rda", compress = "xz")
--- unbias_check.R
+++ unbias_check.R
@@ -1,31 +1,42 @@
 library(latentcor)
-### Data setting
-n <- 1000; p1 <- 15; p2 <- 10 # sample size and dimensions for two datasets.
 
-### Correlation structure within each data set
-set.seed(0)
-perm1 <- sample(1:(p1 + p2), size = p1 + p2);
-Sigma <- autocor(p1 + p2, 0.7)[perm1, perm1]
-mu <- rbinom(p1 + p2, 1, 0.5)
+# Data generation
+rhorep = rep(NA, 100); Rrep = matrix(NA, 100, 3)
+for (rep in 1:100) {
+  rho = runif(1, -1, 1)
+  X = GenData(n = 1000, types = c("ter", "con"), rhos = rho, XP = list(c(.3, .5), NA))
+  R_nc_org = estR(X, types = c("ter", "con"), method = "original")$R
+  R_nc_approx = estR(X, types = c("ter", "con"), method = "approx")$R
+  rhorep[rep] = rho; Rrep[rep, 1] = R_nc_org[2, 1]; Rrep[rep, 2] = R_nc_approx[2, 1]; Rrep[rep, 3] = cor(X)[2, 1]
+}
 
-cp1 <- "exp"; cp2 <- "cube"
+# Plot ternary/continuous case estimation via original method.
+R_nc_org = PlotPair(datapair = cbind(rhorep, Rrep[,1]), namepair = c("True latent correlation", "Estimated latent correlation"),
+         title = "Ternary vs. continuous")
+# Plot ternary/continuous case estimation via approximation method.
+R_nc_approx = PlotPair(datapair = cbind(rhorep, Rrep[,2]), namepair = c("True latent correlation", "Estimated latent correlation (latentcor)"),
+         title = "Ternary vs. continuous")
+R_nc_pearson = PlotPair(datapair = cbind(rhorep, Rrep[,3]), namepair = c("True latent correlation", "Pearson correlation"),
+         title = "Ternary vs. continuous")
+
+cp1 = "exp"; cp2 = "cube"
 for (tp1 in c("continuous", "binary", "ternary", "trunc")) {
   for (tp2 in c("continuous", "binary", "ternary", "trunc")) {
     for (md in c("original", "ml", "approx")) {
-      c1 <- c2 <- NULL
+      c1 = c2 = NULL
       if (tp1 == "binary" | tp1 == "trunc") {
-        c1 <- rep(1, p1)
+        c1 = 1
       } else if (tp1 == "ternary") {
-        c1 <- matrix(rep(1:2, p1), nrow = 2, ncol = p1)
+        c1 = c(1, 2)
       }
       if (tp2 == "binary" | tp2 == "trunc") {
-        c2 <- rep(0, p2)
+        c2 = 0
       } else if (tp2 == "ternary") {
-        c2 <- matrix(rep(0:1, p2), nrow = 2, ncol = p2)
+        c2 = c(0, 1)
       }
-      simdata <- GenData(n=n, copula1 = cp1, copula2 = cp2, type1 = tp1, type2 = tp2,
-                         muZ = mu, Sigma = Sigma, p1= p1, p2 = p2, c1 = c1, c2 = c2)
-      X1 <- simdata$X1; X2 <- simdata$X2
+      simdata = GenData(n=n, type1 = tp1, type2 = tp2, p1= p1, p2 = p2, rho = rho,
+                        copula1 = cp1, copula2 = cp2, c1 = c1, c2 = c2)
+      Sigma = simdata$Sigma; X1 = simdata$X1; X2 = simdata$X2
       assign(paste("R", cp1, cp2, tp1, tp2, md, sep = "_"),
              estR(X1 = X1, type1 = tp1, X2 = X2, type2 = tp2, method = md)$R)
       PlotPair(datapair = cbind(c(Sigma), c(get(paste("R", cp1, cp2, tp1, tp2, md, sep = "_")))),
@@ -34,3 +45,16 @@ for (tp1 in c("continuous", "binary", "ternary", "trunc")) {
     }
   }
 }
+
+
+PlotPair = function(datapair, namepair = c("X", "Y"), title = "Plot X vs Y") {
+  df = data.frame(datapair)
+  colnames(df) = namepair
+  print(ggplot(df, aes(x = datapair[ , 1], y = datapair[ , 2]))
+        + geom_point(color = "blue") + geom_abline(intercept = 0, slope = 1, color = "red")
+        +ggtitle(title) + xlab(namepair[1]) + ylab(namepair[2]))
+}
+
+
+pdf(file = "", width = , height = )
+dev.off()
