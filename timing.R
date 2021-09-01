394dccaf965d48805d8ad69f0d7d724830c0b11e
Mingze Huang <mingzehuang@gmail.com>
2021-09-01 14:48:25 -0500

    Add device and platform information.

--- timing.R
+++ timing.R
@@ -16,4 +16,23 @@ microbenchmark(latentcor(mtcars, types = c("con", "ter", "con", "con", "con", "c
 #  min       lq        mean      median        uq      max    neval
 #  201.9872 215.6438   225.30385 221.5364 226.58330 411.4940   100
 #   71.8457  75.1681   82.42531  80.1688  84.77845 238.3793    100
-               
+
+# My computer parameters (Device)
+
+# Device name	LASB-ECON245-2
+# Full device name	LASB-ECON245-2.CLA.TAMU.EDU
+# Processor	Intel(R) Core(TM) i5-4570 CPU @ 3.20GHz   3.20 GHz
+# Installed RAM	8.00 GB
+# Device ID	66E6F279-2C54-4105-BF32-90259EB9F98A
+# Product ID	00329-00000-00003-AA073
+# System type	64-bit operating system, x64-based processor
+# Pen and touch	No pen or touch input is available for this display
+
+# My computer parameters (Windows specification)
+
+# Edition	Windows 10 Enterprise
+# Version	20H2
+# Installed on	‎5/‎19/‎2021
+# OS build	19042.1165
+# Experience	Windows Feature Experience Pack 120.2212.3530.0
+
